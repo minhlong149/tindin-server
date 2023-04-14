0049503e37d64ba560ada64db1b10bae0127cb55
minhlong149 <minhlong9514@gmail.com>
2023-04-14 08:56:09 +0700

    Initializr Spring Boot project

---
+++ src/main/java/com/mydieu/tindin/TindInApplication.java
@@ -0,0 +1,13 @@
+package com.mydieu.tindin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TindInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TindInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydieu/tindin/TindInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydieu.tindin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TindInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
