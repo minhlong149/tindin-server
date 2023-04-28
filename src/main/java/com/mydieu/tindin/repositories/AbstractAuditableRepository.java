e1417884c80a9859def780788c1bd71c405dfe99
minhlong149 <minhlong9514@gmail.com>
2023-04-28 10:35:15 +0700

    Update repositories

--- src/main/java/com/mydieu/tindin/repositories/AbstractAuditableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mydieu.tindin.repositories;
-
-import org.springframework.data.jpa.domain.AbstractAuditable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface AbstractAuditableRepository<T extends AbstractAuditable, ID> extends JpaRepository<T, ID> {
-}
--- src/main/java/com/mydieu/tindin/repositories/AbstractPersistableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mydieu.tindin.repositories;
-
-import org.springframework.data.jpa.domain.AbstractPersistable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface AbstractPersistableRepository<T extends AbstractPersistable, ID> extends JpaRepository<T, ID> {
-}
