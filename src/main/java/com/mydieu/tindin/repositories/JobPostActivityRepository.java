85375987053d68486969415f5ccb85cf51d912f3
minhlong149 <minhlong9514@gmail.com>
2023-05-30 14:53:27 +0700

    Find applicant applied jobs

--- src/main/java/com/mydieu/tindin/repositories/JobPostActivityRepository.java
+++ src/main/java/com/mydieu/tindin/repositories/JobPostActivityRepository.java
@@ -3,8 +3,16 @@ package com.mydieu.tindin.repositories;
 import com.mydieu.tindin.models.Applicant;
 import com.mydieu.tindin.models.JobPost;
 import com.mydieu.tindin.models.JobPostActivity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+    @Query(value = "SELECT j FROM JobPostActivity jpa JOIN jpa.job j WHERE jpa.applicant = :applicant")
+    List<JobPost> findJobPostsByApplicant(@Param("applicant") Applicant applicant, Pageable pageable);
+
     void deleteByJobIdAndApplicantId(JobPost job, Applicant applicant);
 }
