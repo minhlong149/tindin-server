d2176595440ac074cca4c097bd6a5ec9bc4d3737
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2023-06-04 10:16:47 +0700

    Add recruiter features (#5)
    * Add functionality and error handling to find recruiter by id
    
    * Add functionality to find jobs posted by a recruiter
    
    * Add pagination for job posts
    
    * Add new recruiter
    
    * Update createRecruiter functionality
    
    * Update createRecruiter request body & validation
    
    * Fix exceptions response status code
    
    * Update recruiter info

--- src/main/java/com/mydieu/tindin/repositories/JobPostRepository.java
+++ src/main/java/com/mydieu/tindin/repositories/JobPostRepository.java
@@ -7,14 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+import java.util.List;
+
 public interface JobPostRepository extends JpaRepository<JobPost, Integer> {
     List<JobPost> findByRecruiterId(Integer recruiterId, Pageable pageable);
-
-    //@Query(value = "SELECT jb FROM job_post jb WHERE jb.job_type_id = ?1")
-    //List<JobPost> findByJobTypeId(Integer jobType);
-    //@Query(value = "SELECT jb FROM job_post jb WHERE jb.title= ?1")
+  
     List<JobPost> findByTitle(String title);
-
-
-
 }
