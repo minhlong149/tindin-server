cd43538fb21e6d7e1b52c66d7653078fbc854d88
Qanh195 <qkoanha2nd@gmail.com>
2023-06-24 01:30:36 +0700

    adjust get Organization by name

--- src/main/java/com/mydieu/tindin/repositories/IndustryRepository.java
+++ src/main/java/com/mydieu/tindin/repositories/IndustryRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface IndustryRepository extends JpaRepository<Industry, Integer> {
     Industry findByName (String name);
+
+    Industry findByNameContaining(String name);
 }
